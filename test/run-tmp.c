4bd38d5c1525bf0e7a7ae0a5c3d14d6fa517e014
Oleg Efimov <o.efimov@corp.badoo.com>
2014-09-03 19:47:52 +0400

    test: shorter run-tmp

--- test/run-tmp.c
+++ test/run-tmp.c
@@ -8,127 +8,9 @@
 
 #include "task.h"
 #include "person.pb-c.h"
-#include "test.pb-c.h"
 #include "protobuf2json.h"
 
 int person__debug(void) {
-  int result;
-
-  Foo__Person person = FOO__PERSON__INIT;
-
-  person.name = "John Doe";
-  person.id = 42;
-
-  char *json_string;
-  result = protobuf2json_string(&person.base, TEST_JSON_FLAGS, &json_string, NULL, 0);
-  ASSERT(result == 0);
-  ASSERT(json_string);
-
-  ASSERT_STRCMP(
-    json_string,
-    "{\n"
-    "  \"name\": \"John Doe\",\n"
-    "  \"id\": 42\n"
-    "}"
-  );
-
-  RETURN_OK();
-}
-
-int person__error_unknown_enum_value(void) {
-  int result;
-  char error_string[256] = {0};
-
-  const char *initial_json_string = \
-    "{\n"
-    "  \"name\": \"John Doe\",\n"
-    "  \"id\": 42,\n"
-    "  \"phone\": [\n"
-    "    {\n"
-    "      \"number\": \"+123456789\",\n"
-    "      \"type\": \"UNKNOWN\"\n"
-    "    }\n"
-    "  ]\n"
-    "}"
-  ;
-
-  ProtobufCMessage *protobuf_message = NULL;
-
-  //asm volatile ("int $3");
-
-  result = json2protobuf_string((char *)initial_json_string, 0, &foo__person__descriptor, &protobuf_message, error_string, sizeof(error_string));
-  ASSERT(result == PROTOBUF2JSON_ERR_UNKNOWN_ENUM_VALUE);
-
-  const char *expected_error_string = \
-    "Unknown value 'UNKNOWN' for enum 'Foo.Person.PhoneType'"
-  ;
-
-  printf("Error: %s\n", error_string);
-
-  ASSERT_STRCMP(
-    error_string,
-    expected_error_string
-  );
-
-  return 0;
-}
-
-int list(void) {
-  int result;
-
-  const char *initial_json_string = \
-    "{\n"
-    "  \"numbers\": [\n"
-    "    12,\n"
-    "    34,\n"
-    "    56\n"
-    "  ]\n"
-    "}"
-  ;
-
-  ProtobufCMessage *protobuf_message = NULL;
-
-  result = json2protobuf_string((char *)initial_json_string, 0, &foo__list__descriptor, &protobuf_message, NULL, 0);
-  ASSERT(result == 0);
-
-  Foo__List *list = (Foo__List *)protobuf_message;
-
-  //asm volatile ("int $3");
-
-  printf("Debug: list->n_numbers: %zu\n", list->n_numbers);
-  printf("Debug: list->numbers[0]: %d\n", list->numbers[0]);
-  printf("Debug: list->numbers[1]: %d\n", list->numbers[1]);
-  printf("Debug: list->numbers[2]: %d\n", list->numbers[2]);
-
-  ASSERT(list->n_numbers == 3);
-  ASSERT(list->numbers[0] == 12);
-  ASSERT(list->numbers[1] == 34);
-  ASSERT(list->numbers[2] == 56);
-
-  char *json_string;
-  result = protobuf2json_string(protobuf_message, TEST_JSON_FLAGS, &json_string, NULL, 0);
-  ASSERT(result == 0);
-  ASSERT(json_string);
-
-  const char *expected_json_string = \
-    "{\n"
-    "  \"numbers\": [\n"
-    "    12,\n"
-    "    34,\n"
-    "    56\n"
-    "  ]\n"
-    "}"
-  ;
-
-  ASSERT_STRCMP(
-    json_string,
-    expected_json_string
-  );
-
-  return 0;
-}
-
-int person(void) {
   int result;
 
   const char *initial_json_string = \
@@ -204,10 +86,50 @@ int person(void) {
     expected_json_string
   );
 
+  printf("Debug: OK\n");
+
   return 0;
 }
 
-int list__bad_json_string(void) {
+int person__error_unknown_enum_value(void) {
+  int result;
+  char error_string[256] = {0};
+
+  const char *initial_json_string = \
+    "{\n"
+    "  \"name\": \"John Doe\",\n"
+    "  \"id\": 42,\n"
+    "  \"phone\": [\n"
+    "    {\n"
+    "      \"number\": \"+123456789\",\n"
+    "      \"type\": \"UNKNOWN\"\n"
+    "    }\n"
+    "  ]\n"
+    "}"
+  ;
+
+  ProtobufCMessage *protobuf_message = NULL;
+
+  //asm volatile ("int $3");
+
+  result = json2protobuf_string((char *)initial_json_string, 0, &foo__person__descriptor, &protobuf_message, error_string, sizeof(error_string));
+  ASSERT(result == PROTOBUF2JSON_ERR_UNKNOWN_ENUM_VALUE);
+
+  const char *expected_error_string = \
+    "Unknown value 'UNKNOWN' for enum 'Foo.Person.PhoneType'"
+  ;
+
+  printf("Error: %s\n", error_string);
+
+  ASSERT_STRCMP(
+    error_string,
+    expected_error_string
+  );
+
+  return 0;
+}
+
+int person__bad_json_string(void) {
   int result;
   char error_string[256] = {0};
 
@@ -215,7 +137,7 @@ int list__bad_json_string(void) {
 
   ProtobufCMessage *protobuf_message = NULL;
 
-  result = json2protobuf_string((char *)initial_json_string, 0, &foo__list__descriptor, &protobuf_message, error_string, sizeof(error_string));
+  result = json2protobuf_string((char *)initial_json_string, 0, &foo__person__descriptor, &protobuf_message, error_string, sizeof(error_string));
   ASSERT(result == PROTOBUF2JSON_ERR_CANNOT_PARSE_STRING);
 
   const char *expected_error_string = \
@@ -337,11 +259,7 @@ int main(int argc, char **argv) {
 
   person__error_unknown_enum_value();
 
-  list();
-
-  person();
-
-  list__bad_json_string();
+  person__bad_json_string();
 
   person__error_is_not_array();
 
