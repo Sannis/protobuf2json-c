Sannis/protobuf2json-c

commit 0fbffcb40fd1907fca65340727adb860700604ee

json2protobuf: fix memory leak reported by valgrind

src/protobuf2json.c
@@ -610,8 +610,24 @@ int json2protobuf_process_field(
     }
 
     const char* value_string = json_string_value(json_value);
+    size_t value_string_length = strlen(value_string);
 
-    memcpy(protobuf_value, &value_string, sizeof(value_string));
+    char* value_string_copy = calloc(value_string_length, sizeof(char));
+    if (!value_string_copy) {
+      if (error_string && error_size) {
+        snprintf(
+          error_string, error_size,
+          "Cannot allocate %zu bytes using calloc(3)",
+          value_string_length * sizeof(char)
+        );
+      }
+
+      return PROTOBUF2JSON_ERR_CANNOT_ALLOCATE_MEMORY;
+    }
+
+    memcpy(value_string_copy, value_string, value_string_length);
+
+    *(char **)(protobuf_value) = value_string_copy;
   /*} else if (field_descriptor->type == PROTOBUF_C_TYPE_BYTES) {*/
   /* PROTOBUF_C_TYPE_GROUP - NOT SUPPORTED */
   } else if (field_descriptor->type == PROTOBUF_C_TYPE_MESSAGE) {
@@ -816,6 +832,7 @@ int json2protobuf_string(
     return result;
   }
 
+  json_decref(json_object);
   return 0;
 }
 

test/test-json2protobuf.c
@@ -254,11 +254,16 @@ TEST_IMPL(json2protobuf_string__person__repeated_message) {
   ASSERT(result == 0);
 
   Foo__Person *person = (Foo__Person *)protobuf_message;
+
   ASSERT(person->id == 42);
   ASSERT(person->name);
   ASSERT_STRCMP(person->name, "John Doe");
 
   ASSERT(person->n_phone == 3);
+
+  ASSERT(person->phone[1]->number);
+  ASSERT_STRCMP(person->phone[1]->number, "+987654321");
+  ASSERT(person->phone[1]->type == FOO__PERSON__PHONE_TYPE__MOBILE);
 
   char *json_string;
   result = protobuf2json_string(protobuf_message, TEST_JSON_FLAGS, &json_string, NULL, 0);

commit be82fae3b47f59ac5eb6b04f8597ccd1277f42d3

json2protobuf: fix heap-buffer-overflow reported by asan
Caused by 0fbffcb40fd1907fca65340727adb860700604ee

src/protobuf2json.c
@@ -612,7 +612,7 @@ static int json2protobuf_process_field(
     const char* value_string = json_string_value(json_value);
     size_t value_string_length = strlen(value_string);
 
-    char* value_string_copy = calloc(value_string_length, sizeof(char));
+    char* value_string_copy = calloc(value_string_length + 1, sizeof(char));
     if (!value_string_copy) {
       if (error_string && error_size) {
         snprintf(
@@ -625,7 +625,7 @@ static int json2protobuf_process_field(
       return PROTOBUF2JSON_ERR_CANNOT_ALLOCATE_MEMORY;
     }
 
-    memcpy(value_string_copy, value_string, value_string_length);
+    memcpy(value_string_copy, value_string, value_string_length + 1);
 
     *(char **)(protobuf_value) = value_string_copy;
   /*} else if (field_descriptor->type == PROTOBUF_C_TYPE_BYTES) {*/