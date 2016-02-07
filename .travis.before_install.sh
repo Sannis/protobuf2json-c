66888c7c99233d58279dd49c572ad9e21c5936e9
Oleg Efimov <efimovov@gmail.com>
2016-02-07 13:32:06 +0300

    Use protobuf 2.6.1 and protobuf-c 1.2.1 for test build

--- .travis.before_install.sh
+++ .travis.before_install.sh
@@ -6,18 +6,33 @@
 # and/or modify it under the terms of the MIT license.
 # See LICENSE for details.
 
-echo $TRAVIS_OS_NAME
+echo TRAVIS_OS_NAME=$TRAVIS_OS_NAME
 
+PROTOBUF_VERSION=2.6.1
+echo PROTOBUF_VERSION=$PROTOBUF_VERSION
+
+PROTOBUF_C_VERSION=1.2.1
+echo PROTOBUF_C_VERSION=$PROTOBUF_C_VERSION
+
+echo "Installing protobuf..."
+wget https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-$PROTOBUF_VERSION.tar.gz
+tar xf protobuf-$PROTOBUF_VERSION.tar.gz
+cd protobuf-$PROTOBUF_VERSION && ./configure && make -j2 && sudo make install && sudo ldconfig
+
+echo "Installing protobuf-c..."
+wget https://github.com/protobuf-c/protobuf-c/releases/download/v$PROTOBUF_C_VERSION/protobuf-c-$PROTOBUF_C_VERSION.tar.gz
+tar xf protobuf-c-$PROTOBUF_C_VERSION.tar.gz
+cd protobuf-c-$PROTOBUF_C_VERSION && ./configure && make -j2 && sudo make install && sudo ldconfig
+
+echo "Installing jansson and some tools..."
 if [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
 	brew update
-	brew install protobuf-c
 	brew install jansson
 	brew install cppcheck
 	if [ "x$MY_VALGRIND" = "x1" ]; then brew install valgrind; fi
 else # linux
 	sudo add-apt-repository --yes ppa:pi-rho/security
 	sudo apt-get update
-	sudo apt-get install libprotobuf-c0-dev libprotobuf-c0 protobuf-c-compiler
 	sudo apt-get install libjansson4-dev libjansson4
 	sudo apt-get install cppcheck
 	if [ "x$MY_VALGRIND" = "x1" ]; then sudo apt-get install valgrind; fi
